02e21186d67a681166c8eb2780058906731e231f
Pascal MARTIN <contact@pascal-martin.fr>
2014-06-08 14:39:12 +0200

    modification script resize, nouveaux chemins

--- slides/images/resize.php
+++ slides/images/resize.php
@@ -1,9 +1,11 @@
 #!/usr/bin/env php
 <?php
 
+$orig = "/home/squale/temp/photos-stormtroopers/";
+
 foreach (new GlobIterator(__DIR__ . '/*.jpg') as $file) {
     $filename = $file->getBaseName();
-    $dest = realpath(__DIR__ . '/../') . '/' . $filename;
+    $dest = __DIR__ . '/' . $filename;
     if (!file_exists($dest)) {
         $cmd = sprintf("convert %s -resize 1280x1024\> %s", escapeshellarg($file->getPathname()), escapeshellarg($dest));
         echo $cmd . "\n";
